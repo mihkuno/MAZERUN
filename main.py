6c62cc4295ffb93b4bef543e02bd6e612b83c994
mihkuno <caindayjoeninyo@gmail.com>
2023-12-17 05:41:45 +0800

    optimized generate

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ stack = []
 grid = []        
 cols = None
 rows = None
-w = 50
+w = 10
 isGenerated = False
 
 # global cells
@@ -44,79 +44,12 @@ icon_image = pygame.image.load("./public/icon.png")
 pygame.display.set_icon(icon_image)
 
 
-# ================ Helper functions =================
-
-def moveUp():
-    global current
-    if (current.neighbors[0] is not None) and (current.walls[0] is False):
-        current = current.neighbors[0]
-
-
-def moveRight():
-    global current
-    if (current.neighbors[1] is not None) and (current.walls[1] is False):
-        current = current.neighbors[1]
-
-    
-def moveDown():
-    global current
-    if (current.neighbors[2] is not None) and (current.walls[2] is False):
-        current = current.neighbors[2]
-
-    
-def moveLeft():
-    global current
-    if (current.neighbors[3] is not None) and (current.walls[3] is False):
-        current = current.neighbors[3]
-
-
-def onKeyType(event):
-   global isGenerated
-   # if esc pressed, close program
-   if event.key == pygame.K_ESCAPE:
-      pygame.quit()
-      sys.exit()
-   
-   if isGenerated:
-      if event.key == pygame.K_UP:      moveUp()
-      elif event.key == pygame.K_DOWN:  moveDown()
-      elif event.key == pygame.K_LEFT:  moveLeft()
-      elif event.key == pygame.K_RIGHT: moveRight()
-   
-   else: 
-      print("Key Pressed:", pygame.key.name(event.key))
-      
-
-def onExit():
-   pygame.quit()
-   sys.exit()
-   
-   
-def eventListener():
-   # ==== Mouse Events ====
-   # mouse_x, mouse_y = pygame.mouse.get_pos()
-   
-   for event in pygame.event.get():
-      # ==== On Exit Event ====
-      if event.type == pygame.QUIT:
-         onExit()
-         
-      # ==== Keyboard Events ====
-      elif event.type == pygame.KEYDOWN:
-         onKeyType(event)
-
-
-def index(row, col): # get the one-dimension-grid index of cell 
-    # check if index is beyond the canvas
-    if (row < 0 or col < 0 or row > rows-1 or col > cols-1): 
-        return -1
-    
-    return row + (col * cols)
- 
+# ================ Helper functions ================= 
  
 def drawCurrent():
    global current
    global target
+   global previous
    
    x = current.row * w
    y = current.col * w
@@ -126,15 +59,7 @@ def drawCurrent():
       color = (162, 155, 254) 
    
    pygame.draw.rect(screen, color, (x, y, w, w))
-
-
-def drawTarget():
-   global target
-   x = target.row * w
-   y = target.col * w
-   color = (52, 216, 217)
    
-   pygame.draw.rect(screen, color, (x, y, w, w))
  
 def removeWalls(curr, next):
     x = curr.row - next.row
@@ -194,18 +119,211 @@ def generate():
         target = grid[randIx]
 
 
+def clearCurrent():  
+   global current
+   global w
+   
+   x = current.row * w
+   y = current.col * w
+   
+   # clear the current
+   color = (239, 239, 239) 
+      
+   if current is target:
+      color = (162, 155, 254) 
+   
+   pygame.draw.rect(screen, color, (x, y, w, w))
+   renderEdge(current)
+
+
+def lerp(a, b, t):
+    return a + t * (b - a)
+
+
+def refresh():
+   global previous
+   global current
+   global target
+   
+   
+   # optimize performance 
+   # by only rendering the updated previous block
+   if previous is not None:
+      previous.drawBlock()
+      previous.drawGrid()
+   
+   # assign current to previous
+   previous = current
+   
+   # # draw the target block
+   if target is not None:
+      target.drawTarget()
+      target.drawGrid()
+   
+   # draw the current block 
+   current.drawFocus()
+   current.drawGrid()
+  
+   # draw current neighbor grid
+   for cell in current.neighbors:
+      if cell is not None:
+         cell.drawBlock()
+         cell.drawGrid()
+                
+   # draw target neighbor grid
+   if target is not None:
+      for cell in target.neighbors:
+         if cell is not None:
+            cell.drawBlock()
+            cell.drawGrid()
+            
+
+def moveUp():
+    global current
+    if (current.neighbors[0] is not None) and (current.walls[0] is False):
+        current = current.neighbors[0]
+        refresh()
+
+
+def moveRight():
+    global current
+    if (current.neighbors[1] is not None) and (current.walls[1] is False):
+         current = current.neighbors[1]
+         refresh()
+    
+    
+def moveDown():
+    global current
+    if (current.neighbors[2] is not None) and (current.walls[2] is False):
+         current = current.neighbors[2]
+         refresh()
+    
+    
+def moveLeft():
+    global current
+    if (current.neighbors[3] is not None) and (current.walls[3] is False):
+         current = current.neighbors[3]
+         refresh()
+
+
+def onKeyType(event):
+   global isGenerated
+   # if esc pressed, close program
+   if event.key == pygame.K_ESCAPE:
+      pygame.quit()
+      sys.exit()
+   
+   if isGenerated:
+      if event.key == pygame.K_UP:      moveUp()
+      elif event.key == pygame.K_DOWN:  moveDown()
+      elif event.key == pygame.K_LEFT:  moveLeft()
+      elif event.key == pygame.K_RIGHT: moveRight()
+   
+   else: 
+      print("Key Pressed:", pygame.key.name(event.key))
+      
+
+def onExit():
+   pygame.quit()
+   sys.exit()
+   
+   
+def eventListener():
+   # ==== Mouse Events ====
+   # mouse_x, mouse_y = pygame.mouse.get_pos()
+   
+   for event in pygame.event.get():
+      # ==== On Exit Event ====
+      if event.type == pygame.QUIT:
+         onExit()
+         
+      # ==== Keyboard Events ====
+      elif event.type == pygame.KEYDOWN:
+         onKeyType(event)
+
+
+def index(row, col): # get the one-dimension-grid index of cell 
+    # check if index is beyond the canvas
+    if (row < 0 or col < 0 or row > rows-1 or col > cols-1): 
+        return -1
+    
+    return row + (col * cols)
+ 
+     
 # ================ Cell Class =======================
 
 class Cell:
    def __init__(self, col, row):        
         self.row = row
         self.col = col
-        self.walls = [True,True,True,True] # trbl
+        self.walls = [True,True,True,True] # t-r-b-l
         self.visited = False
 
         # assign cell neighbors on create()       
-        self.neighbors = [None, None, None, None]
+        self.neighbors = [None, None, None, None] # t-r-b-l
    
+   
+   def drawGrid(self):
+      global w
+      
+      x = self.row * w
+      y = self.col * w
+      
+      # handle edge borders    
+      strokeWeight = 3
+      color = (10, 10, 10)
+         
+      # border in sides of cell
+      if self.walls[0]: # tl-tr top
+         pygame.draw.line(screen, color, (x,y), (x+w, y), strokeWeight)    
+      
+      if self.walls[1]: # tr-br right
+         pygame.draw.line(screen, color, (x+w,y), (x+w, y+w), strokeWeight) 
+      
+      if self.walls[2]: # br-bl bottom 
+         pygame.draw.line(screen, color, (x+w,y+w), (x, y+w), strokeWeight) 
+      
+      if self.walls[3]: # bl-tl left
+         pygame.draw.line(screen, color, (x,y+w), (x, y), strokeWeight) 
+         
+      
+   def drawBlock(self):
+      global current
+      global target
+      global w
+      
+      x = self.row * w
+      y = self.col * w
+      
+      if (self is not target) and (self is not current):   
+         color = (239, 239, 239) 
+         pygame.draw.rect(screen, color, (x, y, w, w))
+         
+         
+   def drawFocus(self):
+      global target
+      global w
+      
+      x = self.row * w
+      y = self.col * w
+      color = (235, 77, 75)
+      
+      if self is target:
+         color = (162, 155, 254) 
+
+      pygame.draw.rect(screen, color, (x, y, w, w))
+      
+      
+   def drawTarget(self):
+      global target
+      global w
+      
+      x = target.row * w
+      y = target.col * w
+      color = (52, 216, 217)
+      
+      pygame.draw.rect(screen, color, (x, y, w, w))
+         
    
    def getRandNeighbor(self):
       # randomly select unvisited neighbors
@@ -221,41 +339,7 @@ class Cell:
          randCell = selection[randIx]
          return randCell   
     
-      return None
-   
-   
-   def drawBlock(self):
-      global width, height
-      global w, screen
-      
-      x = self.row * w
-      y = self.col * w
-      color = (200, 200, 200)
-
-      if self.visited:
-         color = (239, 239, 239) 
-      
-      pygame.draw.rect(screen, color, (x, y, width, height))
-   
-   
-   def drawGrid(self):
-      global w, screen
-      strokeWeight = 3
-      x = self.row * w
-      y = self.col * w
-      color = (10, 10, 10)
-   
-      if self.walls[0]: # tl-tr top
-         pygame.draw.line(screen, color, (x,y), (x+w, y),     strokeWeight)     
-      
-      if self.walls[1]: # tr-br right
-         pygame.draw.line(screen, color, (x+w,y), (x+w, y+w), strokeWeight) 
-      
-      if self.walls[2]: # br-bl bottom 
-         pygame.draw.line(screen, color, (x+w,y+w), (x, y+w), strokeWeight) 
-      
-      if self.walls[3]: # bl-tl left
-         pygame.draw.line(screen, color, (x,y+w), (x, y),     strokeWeight)     
+      return None   
       
           
 # ================ Create Maze Grid ==================
@@ -292,7 +376,7 @@ def setup():
       if rCellIx != -1: cell.neighbors[1] = grid[rCellIx]
       if bCellIx != -1: cell.neighbors[2] = grid[bCellIx]
       if lCellIx != -1: cell.neighbors[3] = grid[lCellIx]
-
+      
    # set first cell of grid as current visited
    current = grid[0]
 
@@ -305,39 +389,43 @@ def draw():
    global previous
    global grid
    global isGenerated
+   global height
+   global width
+   global w
    
-   while True:
-      eventListener()
-
-      # Clear the screen
-      color = (255, 255, 255)
-      screen.fill(color)
-
-      # Show all the cell blocks
-      for cell in grid:
-         cell.drawBlock()
-         
-      # Show target cell
-      if target is not None:
-         drawTarget()
-         
-      # Show current cell
-      drawCurrent()
+   # Show all the cell grid and block
+   for cell in grid:
+      cell.drawBlock()
+      cell.drawGrid()
       
-      # Show all the cell grid
-      for cell in grid:
-         cell.drawGrid()
-         
+   
+   # maze render
+   while True:      
+      
       # Generate maze
       if not isGenerated:
          generate()
 
+      refresh()
+            
       # Update the display
       pygame.display.flip()
       
-      # Animate
-      # time.sleep(0.7)
+      if isGenerated:
+         break
       
+      # Animate
+      # time.sleep(0.2)
+   
+   
+   # game control
+   while True:
+      eventListener()
+
+      # Update the display
+      pygame.display.flip()
+      
+     
    
 if __name__ == "__main__":
    setup() 
